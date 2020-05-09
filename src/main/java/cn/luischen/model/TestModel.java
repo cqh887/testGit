ec06106e6a6d8f4a98b014cb52ed5e1bd8a69067
chenqinghao <chenqinghao@wondersgroup.com>
2020-05-09 13:52:35 +0800

    新功能

---
+++ src/main/java/cn/luischen/model/TestModel.java
@@ -0,0 +1,17 @@
+package cn.luischen.model;
+
+/**
+ * Title: <br>
+ * Description: <br>
+ * Company: wondersgroup.com <br>
+ *
+ * @author cqh
+ * @version 1.0
+ */
+public class TestModel {
+
+    /**
+     * 这是新功能
+     */
+
+}

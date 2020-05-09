8c71cc23338c97d55cd80579fac46d0fa4d199e7
chenqinghao <chenqinghao@wondersgroup.com>
2020-05-09 13:44:57 +0800

    添加了cqhDao

---
+++ src/main/java/cn/luischen/model/CqhDao.java
@@ -0,0 +1,30 @@
+package cn.luischen.model;
+
+/**
+ * Title: <br>
+ * Description: <br>
+ * Company: wondersgroup.com <br>
+ *
+ * @author cqh
+ * @version 1.0
+ */
+public class CqhDao {
+    private String name;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}

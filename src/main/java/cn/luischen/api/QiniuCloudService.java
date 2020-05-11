3bd89cbe42e7e368df17ae4a95380954a86b260b
陈庆浩 <chenqinghao@wondersgroup.com>
2020-05-11 08:28:43 +0800

    测试t2

--- src/main/java/cn/luischen/api/QiniuCloudService.java
+++ src/main/java/cn/luischen/api/QiniuCloudService.java
@@ -42,7 +42,7 @@ public class QiniuCloudService {
     @Value("${qiniu.bucket}")
     private String BUCKET;
     /**
-     * 七牛云外网访问                       test
+     * 七牛云外网访问
      */
     @Value("${qiniu.cdn.url}")
     public String QINIU_UPLOAD_SITE;
